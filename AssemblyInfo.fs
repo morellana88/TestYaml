b57d7868d244201f93d20c26fd027bab9606e1cf
Marco Orellana <marco_orellana@me.com>
2015-07-15 01:16:17 -0600

    Initial Commit

---
+++ AssemblyInfo.fs
@@ -0,0 +1,20 @@
+﻿namespace YalmTest
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("YalmTest")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("marco")>]
+[<assembly: AssemblyTrademark("")>]
+
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
+()
---
+++ Program.fs
@@ -0,0 +1,37 @@
+﻿open System
+open System.Collections.Generic
+open YamlDotNet.Serialization
+open YamlDotNet.Serialization.NamingConventions
+open System.IO
+
+type Backend() = 
+    member val TimeSeries = "" with get, set
+    member val DataPoints = "" with get, set
+
+type Postgres() =
+    member val ConnectionString  = "" with get,set
+    member val PoolSize = 0 with get,set
+
+type Hbase() =
+    member val Host = "" with get, set
+    member val Port = 0 with get, set
+    member val PoolSize = 0 with get, set
+    member val BufferSize = 0 with get, set
+
+type Zmq() = 
+    member val Endpoint = "" with get, set
+    member val Listener = "" with get, set
+
+type Configuration() = 
+    member val Backend = new Backend() with get,set
+    member val Postgres = new Postgres() with get,set
+    member val Hbase = new Hbase() with get, set
+    member val Zmq = new Zmq() with get, set
+
+let deserializer = new Deserializer(namingConvention = new CamelCaseNamingConvention())
+let stream = File.OpenText("../../config.yaml")
+let config = deserializer.Deserialize<Configuration>(stream)
+stream.Close();
+
+Console.WriteLine(config.Hbase.BufferSize)
+Console.WriteLine(config.Postgres.ConnectionString)
